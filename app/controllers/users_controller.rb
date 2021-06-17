7cee9b39e0d132dd7fb2495c6b465f742bd6cb87
Sam Head <shead@cteh.com>
2021-06-17 14:27:45 -0500

    add helper methods and DRY users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
  
+  before_action :set_user, only: [:show, :edit, :update]
+
   def new
     @user = User.new
   end
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
 
   end
@@ -15,30 +16,35 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
-        flash[:notice] = "Your account has been updated."
-        redirect_to @user
+      flash[:notice] = "Your account has been updated."
+      redirect_to @user
     else
-        render "edit"
+      render "edit"
     end
   end
   
   def create 
     @user = User.new(user_params)
     if @user.save
-        flash[:notice] = "Welcome to the Alpha Blog #{@user.username}. It only gonna get worse :D"
-        redirect_to articles_path
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}. It only gonna get worse :D"
+      redirect_to articles_path
     else
-        render 'new'
+      render 'new'
     end
   end
+
   private
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
