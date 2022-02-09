0f342b8860436102ae35820fd8aadaf33e0459c4
Sam Head <shead@cteh.com>
2022-02-09 14:31:47 -0600

    Add colorization to flash and admin stuff

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -54,7 +54,7 @@ class ArticlesController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @article.user
+    if current_user != @article.user && !current_user.admin?
       flash[:alert] = "This does not belong to you."
       redirect_to @article
     end
